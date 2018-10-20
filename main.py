a0a30aaa859b773827492920ddfd6d3055728a9d
benzach0421 <benzach0421@gmail.com>
2018-10-20 14:33:51 -0600

    Added grade inputs for classes. The number of classes will be variable in the future, this is just a prototype

---
+++ main.py
@@ -0,0 +1,11 @@
+#!/bin/bash/python3
+
+print ("Enter your grades, (percent): \n")
+
+one = input ("1st class: ")
+two = input ("2nd class: ")
+three = input ("3rd class: ")
+four = input ("4th class: ")
+five = input ("5th class: ")
+six = input ("6th class: ")
+seven = input ("7th class: ")
