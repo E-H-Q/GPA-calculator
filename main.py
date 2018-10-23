707c1042c2717fd92d9169c1ca16ccc072d26fb7
Ben Zacharias <benzach0421@gmail.com>
2018-10-22 21:31:22 -0600

    Drastically shortened length of file, using lists instead of a bunch of set variables.

--- main.py
+++ main.py
@@ -1,23 +1,15 @@
 #!/bin/bash/python3
 
-print ("Enter your grades, (percent): \n")
+per_num = int(input ("How many classes do you have?: "))
+pers = [None] * per_num
+i = 0
 
-one = int(input ("1st class: "))
-two = int(input ("2nd class: "))
-three = int(input ("3rd class: "))
-four = int(input ("4th class: "))
-five = int(input ("5th class: "))
-six = int(input ("6th class: "))
-seven = int(input ("7th class: "))
+print ("\nEnter your grades, (percentages): ")
 
-one /= (20 - 1)
-two /= (20 - 1)
-three /= (20 -1)
-four /= (20 - 1)
-five /= (20 - 1)
-six /= (20 - 1)
-seven /= (20 - 1)
+while (i < len(pers)):
+    pers[i] = int(input ("  Period " + str(i+1) + ": ")) / 20 - 1
+    i += 1
 
-total = one + two + three + four + five + six + seven
-gpa = total / 7
-print (round(gpa,1))
+total = sum(pers)
+gpa = total / (len(pers))
+print ("\nGPA:",round(gpa,1))
